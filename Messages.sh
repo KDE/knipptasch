7609fc59b958c87910b792f1e72d5ee8ba52a527
Albert Astals Cid <aacid@kde.org>
2011-01-29 12:31:43 +0000

    Fix Messages.sh
    CCMAIL: kde@hilefoks.org

--- Messages.sh
+++ Messages.sh
@@ -3,5 +3,5 @@
 $EXTRACTRC `find . -name '*.ui' \
             -o -name '*.rc' \
             -o -name '*.kcfg'` >> rc.cpp
-$XGETTEXT src/main.cpp rc.cpp -o $podir/knipptasch.pot
+$XGETTEXT gui/main.cpp rc.cpp -o $podir/knipptasch.pot
 $XGETTEXT_QT `find . -name '*.cpp' -a ! -name main.cpp` -o $podir/knipptasch_qt.pot
