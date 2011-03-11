a9a2c4cc9556212bd8744beb9ab59eed8070ab22
Stefan Böhmann <kde@hilefoks.org>
2011-03-11 15:54:18 +0100

    update scripts/codingstyle.sh

--- scripts/codingstyle.sh
+++ scripts/codingstyle.sh
@@ -1,24 +1,4 @@
 #!/bin/sh
 
 echo "Applying astyle rules..."
-astyle \
---indent=spaces=4 \
---brackets=linux \
---indent-classes \
---indent-switches \
---indent-cases \
---indent-namespaces \
---indent-preprocessor \
---indent-labels \
---pad=oper \
---pad=paren-in \
---unpad=paren \
---one-line=keep-statements \
---convert-tabs \
-`find src/ -type f -name '*.cpp'` `find src/ -type f -name '*.h'`
-
-echo "Removing .orig files..."
-find src/ -type f -name '*.orig' -exec rm {} \;
-
-echo -e "\n [done]\n"
-
+astyle --indent=spaces=4 --brackets=linux --indent-classes --indent-switches --indent-cases --indent-namespaces --indent-preprocessor --indent-labels --pad=oper --pad=paren-in --unpad=paren --one-line=keep-statements --convert-tabs --add-brackets --align-pointer=name --suffix=none --exclude=build --exclude=3rdparty --lineend=linux --recursive --verbose *.h *.cpp
