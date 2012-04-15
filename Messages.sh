99f539c85bd52beba8a45773f17dbaa77f398747
Pino Toscano <pino@kde.org>
2012-04-15 16:07:26 +0200

    i18n: do not extract messages from test and examples subdirs of qsint

--- Messages.sh
+++ Messages.sh
@@ -2,6 +2,6 @@
 
 $EXTRACTRC `find . -name '*.ui' \
             -o -name '*.rc' \
-            -o -name '*.kcfg'` >> rc.cpp
+            -o -name '*.kcfg' | grep -v '/qsint/test/' | grep -v '/qsint/examples/'` >> rc.cpp
 $XGETTEXT gui/main.cpp rc.cpp -o $podir/knipptasch.pot
 $XGETTEXT_QT `find . -name '*.cpp' -a ! -name main.cpp` -o $podir/knipptasch_qt.pot
